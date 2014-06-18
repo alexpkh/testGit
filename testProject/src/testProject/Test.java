a47a7ec9ee9ac07628d709f255b305bd535c27be
ghpark <ghpark@iwki.co.kr>
2014-06-18 14:31:43 +0900

    aaaa

--- testProject/src/testProject/Test.java
+++ testProject/src/testProject/Test.java
@@ -4,5 +4,5 @@ public class Test {
 	String test ="TESTaaaaa12dddddddssssssssdddddd12a"; 
 	String tes2t ="TESTaaaaa12dddddddssssssssdddddd12a"; 
 	String test3 ="TESTaaaaa12dddddssddsssssasassfdsssdddddd12a"; 
-	String test4 ="4"; 
+	String tessst4 ="4"; 
 }
