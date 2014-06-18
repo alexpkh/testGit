b5650eda9d89c9f2745140aed38602e4aebddf04
ghpark <ghpark@iwki.co.kr>
2014-06-18 14:22:18 +0900

    sss

--- testProject/src/testProject/Test.java
+++ testProject/src/testProject/Test.java
@@ -3,5 +3,5 @@ package testProject;
 public class Test {
 	String test ="TESTaaaaa12dddddddssssssssdddddd12a"; 
 	String tes2t ="TESTaaaaa12dddddddssssssssdddddd12a"; 
-	String test3 ="TESTaaaaa12dddddssddsssssasfdsssdddddd12a"; 
+	String test3 ="TESTaaaaa12dddddssddsssssasassfdsssdddddd12a"; 
 }
